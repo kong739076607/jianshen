a339f9293d34c4ab7a53d7da67d8cdc6cfb0adf6
firstnumberone <IT739076607@163.com>
2017-12-10 22:07:49 +0800

    静态提交

---
+++ kxc (1)/homePage.js
@@ -0,0 +1,18 @@
+
+			var li1=document.getElementById("li1");
+			var xiala=document.getElementById("xialanav");
+			li1.onmousemove=function(){
+				xiala.style.display="block";
+				
+			}
+			li1.onmouseout=function(){
+				xiala.style.display="none";
+			}
+			xiala.onmousemove=function(){
+				xiala.style.display="block";
+				
+			}
+			xiala.onmouseout=function(){
+				xiala.style.display="none";
+			}
+		
